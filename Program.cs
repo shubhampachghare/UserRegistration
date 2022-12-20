3d6df4c3a61029ab260d9eccd69cc75850d7128d
shubhampachghare <shubhampachghare@gmail.com>
2022-12-20 21:54:13 +0530

    [Shubham]-Welcome to user registration

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UserRegistration
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to the user registration ");
+        }
+    }
+}
